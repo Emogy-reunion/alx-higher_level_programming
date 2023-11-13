e5c8827099302d5ca235b26ba9992fbf9e6e2b68
Emogy-reunion <mv7786986@gmail.com>
2023-11-13 13:56:40 -0800

    second commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for 1digit in range(0, 10):
     for 2digit in range(1digit + 1, 10):
         if 1digit == 8 and 2digit == 9:
-            print("{}{}".format(1digit, 2digit))
+            print("{:d}{:d}".format(1digit, 2digit))
         else:
-            print("{}{}".format(1digit, 2digit), end=", ")
+            print("{:d}{:d}".format(1digit, 2digit), end=", ")
