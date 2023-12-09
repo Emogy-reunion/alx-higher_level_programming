fc30156c407a5de48380fd8c8536b4acca6db05e
Emogy-reunion <mv7786986@gmail.com>
2023-12-09 04:23:56 -0800

    removed an extra line

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    """computes the square value of all integers of a matrix using map"""
     return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
