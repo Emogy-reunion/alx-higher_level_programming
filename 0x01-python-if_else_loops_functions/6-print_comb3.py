447fd84171e9a65314bf785a13aaa97167704906
Emogy-reunion <mv7786986@gmail.com>
2023-12-09 04:31:56 -0800

    change some few stuff

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """print all possible combinations of two numbers"""
-for 1digit in range(0, 10):
-    for 2digit in range(1digit + 1, 10):
-        if 1digit == 8 and 2digit == 9:
-            print("{}{}".format(1digit, 2digit))
-        else:
-            print("{}{}".format(1digit, 2digit), end=", ")
+for j in range(0, 8):
+    for k in range(j + 1, 10):
+        print("{:d}{:d}".format(j, k), end=', ')
+print("{:d}{:d}".format(j + 1, k))
