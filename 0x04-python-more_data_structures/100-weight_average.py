dbb24b6c354da39dfc17b2ee98cfba3a31130d5d
Emogy-reunion <mv7786986@gmail.com>
2023-12-09 04:16:32 -0800

    changed some stuff

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     """returns the weighted average of all integers tuple (<score>, <weight>)"""
-    if not my_list:
-        return 0
-    average = 0
-    div = 0
-    for tup in my_list:
-        average += tup[0] * tup[1]
-        div += tup[1]
-    return float(average / div)
+    if my_list and len(my_list):
+        num = 0
+        dem = 0
+        for tup in my_list:
+            num += (tup[0] * tup[1])
+            dem += tup[1]
+        return (num / dem)
+    return 0
