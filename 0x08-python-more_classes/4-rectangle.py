e8979932d128f097f92c8c68c510f48d4e1da3cf
Emogy-reunion <mv7786986@gmail.com>
2023-12-29 06:27:57 -0800

    minor changes

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -77,4 +77,4 @@ class Rectangle:
     def __repr__(self):
         """method to provide a string representation for
         recreation"""
-        return f"Rectangle(width={self.__width}, height={self.__height})"
+        return f"Rectangle({self.__width}, {self.__height})"
