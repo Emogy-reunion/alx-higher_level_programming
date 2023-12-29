a34215cbd61548108ba061b3a8336b6be4d53311
Emogy-reunion <mv7786986@gmail.com>
2023-12-29 05:44:09 -0800

    minor changes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """sets the width"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -45,7 +45,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """sets the height"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
