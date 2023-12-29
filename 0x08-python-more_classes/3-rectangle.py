f2b3eae5c66d32b1cf991360643fee126b638d05
Emogy-reunion <mv7786986@gmail.com>
2023-12-29 05:50:23 -0800

    minor changes

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -26,32 +26,30 @@ class Rectangle:
     @property
     def width(self):
         """Retrieves the width"""
-        return __width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """sets the width"""
         if not isinstance(value, int):
-            raise TypeError("Width must be an integer")
-        elif value < 0:
+            raise TypeError("width must be an integer")
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
         """Retrieves the height"""
-        return __height
+        return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
         """sets the height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
         """calculates and returns the area of the rectangle"""
@@ -63,7 +61,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            the_perimeter = 2 * (self.__width * self.__length)
+            the_perimeter = 2 * (self.__width + self.__height)
             return the_perimeter
 
         def __str__(self):
