3e46fab9a510fe262393ade11e1aa8f277915ac7
Emogy-reunion <mv7786986@gmail.com>
2023-12-29 16:25:56 -0800

    minor changes

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -29,32 +29,30 @@ class Rectangle:
     @property
     def width(self):
         """Retrieves the width"""
-    return __width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """sets the width"""
         if not isinstance(value, int):
             raise TypeError("Width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
         """Retrieves the height"""
-        return __height
+        return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
         ""sets the height""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
         """calculates and returns the area of the rectangle"""
@@ -66,17 +64,17 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            the_perimeter = 2 * (self.__width * self.__length)
+            the_perimeter = 2 * (self.__width + self.__length)
             return the_perimeter
 
     def __str__(self):
         """method to provide a string representation of rectamgle"""
-        rect_str = " "
+        rect_str = ""
         if self.__width == 0 or self.__height == 0:
             return rect_str
         else:
             for _ in range(self.__height):
-                rect_str += str(Rectangle.print_symbol) * self._width + '\n'
+                rect_str += str(Rectangle.print_symbol) * self.__width + '\n'
             return rect_str.strip()
 
     def __repr__(self):
