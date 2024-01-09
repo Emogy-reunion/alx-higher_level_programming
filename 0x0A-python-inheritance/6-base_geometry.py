0a34abe0b30f3b5fe12efb285dddbb92f1a17bb3
Emogy-reunion <mv7786986@gmail.com>
2024-01-09 01:12:20 -0800

    first commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    base class for geometry operations
+    """
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
