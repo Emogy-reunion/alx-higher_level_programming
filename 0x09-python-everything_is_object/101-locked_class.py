c2dc7ff9cf4883a09b2612701130189a7a959e36
Emogy-reunion <mv7786986@gmail.com>
2024-01-08 20:15:42 -0800

    first commit

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+class LockedClass:
+    __slots__ = ('first_name',)
+
+    def __setattr__(self, name, value):
+        if name != 'first_name':
+            raise AttributeError(f"{self.__class__.__name__} has no attribute '{name}'")
+        super().__setattr__(name, value)
