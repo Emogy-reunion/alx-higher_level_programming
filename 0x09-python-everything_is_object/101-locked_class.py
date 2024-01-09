fe90e1cf279b5988e1a30f090b5dbb898caa83b9
Emogy-reunion <mv7786986@gmail.com>
2024-01-08 21:18:05 -0800

    first commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""class that locks creation of dynamic attributes"""
+"""class that locks creation of dynamic attributes
+    unless it's called first_name"""
 
 
 class LockedClass:
     """a locked class"""
-    __slots__ = ('first_name',)
-
-    def __setattr__(self, name, value):
-        """locks the class"""
-        if name != 'first_name':
-            raise AttributeError(f"{self.__class__.__name__} has no attribute '{name}'")
-        super().__setattr__(name, value)
+    __slots__ = ["first_name"]
