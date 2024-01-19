00d7901f6db6820636937281f02dfac16577ec82
Emogy-reunion <mv7786986@gmail.com>
2024-01-18 16:21:40 -0800

    first commit

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Records should be ordered by score (top first) 
+-- The database name will be passed as an argument of the mysql command
+SELECT score, name FROM second_table
+ORDER BY DESC;
