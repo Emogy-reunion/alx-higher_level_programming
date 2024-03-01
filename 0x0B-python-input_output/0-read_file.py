6775eb66c11cd568c5f830481846006ba21e73d1
Emogy-reunion <mv7786986@gmail.com>
2024-03-01 04:09:59 -0800

    first commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,8 +4,9 @@
 
 def read_file(filename=" "):
     """
-    param file_name: path to the file
+    Args:
+        :filename: path to the file
     """
-    with open(filename, "r", encoding='UTF-8') as file:
+    with open(filename, "r", encoding='utf-8') as file:
         data = file.read()
         print(data, end=" ")
