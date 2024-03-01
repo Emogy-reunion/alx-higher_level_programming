9229d42058046b9211013a04c91c9d5634e5ce4a
Emogy-reunion <mv7786986@gmail.com>
2024-02-29 16:36:22 -0800

    minor changes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,10 +2,10 @@
 """handles reading data from a file"""
 
 
-def read_file(file_name=""):
+def read_file(filename=" "):
     """
     param file_name: path to the file
     """
-    with open(file_name, "r", encoding='UTF-8') as file:
+    with open(filename, "r", encoding='UTF-8') as file:
         data = file.read()
         print(data, end=" ")
