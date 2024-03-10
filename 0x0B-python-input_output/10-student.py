4fcbc0af4ac62dd51a595e62e787eae45eda0835
Emogy-reunion <mv7786986@gmail.com>
2024-03-09 21:03:43 -0800

    first commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str): student's first name
+            last_name (str): student's last name
+            age (int): student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """that retrieves a dictionary representation of a Student instance
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {m: getattr(self, m) for m in attrs if hasattr(self, m)}
+        return self.__dict__
