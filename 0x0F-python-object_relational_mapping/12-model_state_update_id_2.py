504698ef1f1c5918aa308eafb550676a64df78be
Emogy-reunion <mv7786986@gmail.com>
2024-03-22 15:45:50 -0700

    first commit

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""adds the State object “Louisiana” to the database hbtn_0e_6_usa"""
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    """maps class"""
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter_by(id=2)
+    instance = query.first()
+    if instance:
+        instance.name = "New Mexico"
+    session.commit()
